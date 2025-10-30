73687c78a1dc2e8544731177b0e1085ce43bbdde
נטע כנרי <netakanary@MacBook-Pro-sl-nt.local>
2025-10-30 20:23:31 +0200

    fix bill3

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
 		double splitbill = Math.ceil((double) bill / 3);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + splitbill + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + splitbill + " Shekels each");
 
 	}
 }
